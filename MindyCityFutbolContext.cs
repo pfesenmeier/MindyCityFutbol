2878996482800c764209bfcd43c0f18bf10c6b48
Paul Fesenmeier <pfesenmeier@gmail.com>
2023-02-28 10:34:28 -0500

    scaffold aspnet, db context

---
+++ MindyCityFutbolContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MindyCityFutbol;
+
+public partial class MindyCityFutbolContext : DbContext
+{
+    public MindyCityFutbolContext()
+    {
+    }
+
+    public MindyCityFutbolContext(DbContextOptions<MindyCityFutbolContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("host=localhost;port=5432;database=MindyCityFutbol;username=postgres;password=password;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ TODO.md
@@ -0,0 +1,16 @@
+# do everything the easy way
+# except for the security-related bits
+# terraform setup... easy and hard way
+# terraform template with all entities from example
+# running migrations... simple and the hard way
+
+- Postgres'ing the example
+- Getting REST up and running locally
+- Getting deployed to Azure unsafely
+- Getting deployed to Azure safely
+- Terraforming
+
+List:
+Added Postgres to dependencies
+  - Add Microsoft.EntityFrameworkCore.Design package for migration createion (I think)
+  - Add Npgsql.EntityFrameworkCore.PostgreSQL pckg
